e3b406ad008fcfae6cd306fca417f4b228b199b1
Jeremy Khalfa <khalfa.jeremy@gmail.com>
2025-12-07 08:29:00 +0200

    Add Puma Script

--- config/puma.rb
+++ config/puma.rb
@@ -32,3 +32,31 @@ plugin :tmp_restart
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Specifies the number of `workers` to boot in clustered mode
+# Workers are forked web server processes. If using threads and workers together,
+# the concurrency of the application would be max `threads` * `workers`.
+workers ENV.fetch("WEB_CONCURRENCY", 2)
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+preload_app!
+
+# Allow puma to be restarted by `rails restart` command
+plugin :tmp_restart
+
+# Bind to unix socket for Nginx (production)
+if ENV["RAILS_ENV"] == "production"
+  bind "unix://#{ENV.fetch('PUMA_SOCKET', 'tmp/sockets/puma.sock')}"
+end
+
+# Enable systemd notify support
+if ENV["NOTIFY_SOCKET"]
+  on_booted do
+    require 'sd_notify'
+    SdNotify.ready
+  end
+end
+
