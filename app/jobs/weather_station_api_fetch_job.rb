41ddc03d213b1af65602f4d8a680266bcbef7ce7
Jeremy Khalfa <khalfa.jeremy@gmail.com>
2026-03-01 01:57:11 +0200

    Increase to 6 seconds for delays between queries

--- app/jobs/weather_station_api_fetch_job.rb
+++ app/jobs/weather_station_api_fetch_job.rb
@@ -3,13 +3,11 @@ class WeatherStationApiFetchJob
   queue_as :low
   sidekiq_options retry: 3
 
-  # Periodic job: discovers active weather station API locations that need fetching
-  # and enqueues individual fetch jobs for each.
   def perform
     weather_station_api_locations = WeatherStationApiLocation.needs_fetch
 
     weather_station_api_locations.find_each.with_index do |weather_station_api_location, index|
-      WeatherStationApiLocationFetchJob.perform_in(index * 4, weather_station_api_location.id)
+      WeatherStationApiLocationFetchJob.perform_in(index * 6, weather_station_api_location.id)
     end
   end
 end
