c6a77d7bd817c14de309f04fa76058d5393cd782
Jeremy Khalfa <khalfa.jeremy@gmail.com>
2026-03-29 01:24:16 +0300

    Set Google Maps API Key In Env

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:jerems6/webApplication.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/credentials/production.key", "config/application.yml"
+append :linked_files, "config/database.yml", "config/credentials/production.key", "config/application.yml", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
