44cd53385756edbdf97b0c8b5a358ecc5ea756f0
Jeremy Khalfa <khalfa.jeremy@gmail.com>
2026-04-02 22:24:51 +0300

    Fix Plc Polling: Joins to Measurement Points

--- app/jobs/plc_polling_job.rb
+++ app/jobs/plc_polling_job.rb
@@ -4,6 +4,7 @@ class PlcPollingJob
 
   def perform
     plcs = Plc.operational
+      .joins(:measurement_points)
       .where(measurement_points: { active: true, data_collection_enabled: true })
       .distinct
 
