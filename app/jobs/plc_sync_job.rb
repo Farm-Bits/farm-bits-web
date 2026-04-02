182a1b0d9cf08cf21bd46b4193b6467b8a95b241
Jeremy Khalfa <khalfa.jeremy@gmail.com>
2026-04-03 00:50:22 +0300

    Add Io Active To Sync Job

--- app/jobs/plc_sync_job.rb
+++ app/jobs/plc_sync_job.rb
@@ -6,6 +6,7 @@ class PlcSyncJob
     Plc.operational.find_each do |plc|
       PlcClockWriteJob.perform_async(plc.id)
       SunDataPlcWriteJob.perform_async(plc.id)
+      IoActiveSyncJob.perform_async(plc.id)
       OperationModeSyncJob.perform_async(plc.id)
     end
   end
